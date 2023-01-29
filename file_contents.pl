7efa06d69d90c17996aa8426e5eb81cc1ee0c03a
Justin Fay <justin.fay@hope.edu>
2023-01-29 16:38:51 -0500

    file_contents done

--- file_contents.pl
+++ file_contents.pl
@@ -1,3 +1,33 @@
 use strict;
 use warnings;
 
+my @objects;
+
+if(@ARGV == 1){
+# take in file name
+# Grab Author using git cat file
+# Print commmit number 
+# cat the file
+my $file = $ARGV[0];
+my $commit;
+chomp(@objects = `git log -1 --name-only $file`);
+for(my $i = 0; $i < $#objects; $i++){
+    my @val = split / /, $objects[$i];
+    if($i == 0){
+        $commit = $val[1];
+    } 
+    elsif ($i == 1) {
+        print($objects[$i] . "\n\n"); 
+    }
+}
+
+print("SHA-1 for " . $file . ": " . $commit . "\n");
+chomp(my @print = `cat $file`);
+for my $line (@print){
+print("\n". $line . "\n");
+}
+
+} else {
+    print("Incorrect Command. \n");
+    exit -1;
+}
