ba22db2c52a39682547f86572e10c6f95b37c978
Justin Fay <justin.fay@hope.edu>
2023-01-30 15:31:13 -0500

    Pretty much got the dates

--- contributions.pl
+++ contributions.pl
@@ -1,24 +1,21 @@
 use strict;
 use warnings;
-#use DateTime;
 
-sub getDate {
-    #$unix_timestamp = '1461241125.31307';
-    #my $dt = DateTime->from_epoch(epoch => $unix_timestamp);
-    #print $dt->strftime('%Y-%m-%d %H:%M:%S'),"\n";
-}
+#git log --pretty=%ad --author=justin.fay@hope.edu --date=unix -1 //Most Recent
+# git log --pretty=%ad --author=justin.fay@hope.edu --date=unix --reverse //Odest
 
 my @authorsEmails;
 my %emailCount;
 chomp(@authorsEmails = `git log --pretty=%ae`);
 for my $email (@authorsEmails){
-    if(exists($emailCount{$email})){
-        $emailCount{$email}++;
-    } else {
-        $emailCount{$email} = 1;
-    }
+    if(exists($emailCount{$email})) { $emailCount{$email}++; } 
+    else                            { $emailCount{$email} = 1; }
 }
 
 for my $key (keys %emailCount){
-    print($key . ", " . $emailCount{$key} . "\n");
+   chomp(my @oldestDate = `git log --date=unix --pretty=%cd --author=justin.fay\@hope.edu --reverse`);
+   chomp(my $recentDate = `git log --date=unix --pretty=%cd --author=justin.fay\@hope.edu -1`);
+   print($key . ", " . $emailCount{$key} . ", " . (scalar localtime $oldestDate[0]) . ", " . (scalar localtime $recentDate) . "\n")
+
+
 }
