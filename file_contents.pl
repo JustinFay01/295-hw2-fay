f0542b02b9f2f9dbe7bb69fa4732467a01e001d9
Justin Fay <justin.fay@hope.edu>
2023-01-29 19:30:46 -0500

    Fixed printing extra newlines when printing the file

--- file_contents.pl
+++ file_contents.pl
@@ -3,14 +3,12 @@ use warnings;
 
 my @objects;
 
-if(@ARGV == 1){
-# take in file name
-# Grab Author using git cat file
-# Print commmit number 
-# cat the file
+if(@ARGV == 1){ 
 my $file = $ARGV[0];
 my $commit;
+#Find objects associated with file
 chomp(@objects = `git log -1 --name-only $file`);
+#find commit and author
 for(my $i = 0; $i < @objects; $i++){
     my @val = split / /, $objects[$i];
     if($i == 0){
@@ -20,11 +18,12 @@ for(my $i = 0; $i < @objects; $i++){
         print($objects[$i] . "\n\n"); 
     }
 }
-
-print("SHA-1 for " . $file . ": " . $commit . "\n");
+#print file name and sha
+print("SHA-1 for " . $file . ": " . $commit . "\n\n");
+#print file
 chomp(my @print = `cat $file`);
 for my $line (@print){
-print("\n". $line . "\n");
+print($line . "\n");
 }
 
 } else {
